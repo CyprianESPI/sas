e5b1223d0145a822763bb5325ce9fa3d22e4db6e
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-09 13:49:58 +0200

    Use card component with ng-container

--- src/app/pages/full-custom-page/full-custom-page.component.ts
+++ src/app/pages/full-custom-page/full-custom-page.component.ts
@@ -1,14 +1,25 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule, NgComponentOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Type } from '@angular/core';
+import { CardComponent } from '../../common/card/card.component';
+
+interface CustomComponent {
+  component: Type<any>;
+  inputs?: Record<string, any>;
+}
 
 @Component({
   selector: 'app-full-custom-page',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>full-custom-page works!</p>`,
+  imports: [CommonModule],
+  template: `<p>full-custom-page works!</p>
+    <ng-container *ngComponentOutlet="custom.component; inputs: custom.inputs">
+    </ng-container>`,
   styleUrl: './full-custom-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FullCustomPageComponent { }
+export class FullCustomPageComponent {
+  custom: CustomComponent = {
+    component: CardComponent,
+    inputs: { title: 'Title', content: 'This is the content of the card' },
+  };
+}
