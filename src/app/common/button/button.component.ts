16326df8473c7ef56946223fb4ad5eeaed934dd6
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-09 16:28:15 +0200

    Add onClick event to button

--- src/app/common/button/button.component.ts
+++ src/app/common/button/button.component.ts
@@ -1,15 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [CommonModule],
-  template: `<button>{{ content }}</button>`,
+  template: `<button (click)="onClick()">{{ content }}</button>`,
   styleUrl: './button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
   @Input()
   content: string = '';
+
+  @Output() buttonClicked = new EventEmitter<void>(); // Emitting an event without any data
+
+  onClick(): void {
+    console.log(`onClick(${this.content})`);
+    this.buttonClicked.emit(); // Emitting the click event
+  }
 }
