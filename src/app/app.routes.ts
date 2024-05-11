9b7ed2b5d78e7e9b26b7596738bc51b2bd20906b
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-11 15:51:27 +0200

    Add toolbar page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { FullCustomPageComponent } from './pages/full-custom-page/full-custom-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CardPageComponent } from './pages/card-page/card-page.component';
+import { ToolbarPageComponent } from './pages/toolbar-page/toolbar-page.component';
 
 export const codeSnippetsRoutes: Routes = [
   { path: 'full-custom', component: FullCustomPageComponent },
   { path: 'card', component: CardPageComponent },
+  { path: 'toolbar', component: ToolbarPageComponent },
 ];
 
 export const routes: Routes = [
---
+++ src/app/pages/toolbar-page/toolbar-page.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IComponentData } from '../../models/i-component';
+import { ISourceCode } from '../../models/i-source-code';
+import { ToolbarComponent } from '../../common/toolbar/toolbar.component';
+import { ShowCaseComponent } from '../../common/show-case/show-case.component';
+
+@Component({
+  selector: 'app-toolbar-page',
+  standalone: true,
+  imports: [CommonModule, ShowCaseComponent, ToolbarComponent],
+  template: `<app-show-case [cmp]="cmp" [sources]="sources"></app-show-case>`,
+  styleUrl: './toolbar-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarPageComponent {
+  cmp: IComponentData = ToolbarComponent.Make({
+    buttons: [{ callBack: () => {}, content: 'clickMe', toolTip: 'helpMe' }],
+    subTitle: 'to the jungle',
+    title: 'Welcome',
+  });
+  sources: ISourceCode[] = [
+    {
+      name: 'example.html',
+      code: `<app-toolbar
+      [data]="{
+        buttons: [
+          { callBack: () => {}, content: 'clickMe', helpMe: '' }
+        ],
+        subTitle: 'to the jungle',
+        title: 'Welcome'
+      }"
+    ></app-toolbar>`,
+    },
+    {
+      name: 'models/i-component.ts',
+    },
+    {
+      name: 'common/toolbar/toolbar.component.ts',
+    },
+    {
+      name: 'common/toolbar/toolbar.component.scss',
+    },
+  ];
+}
