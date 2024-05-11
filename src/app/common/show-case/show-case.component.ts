225e2d902fd6f4da9b28bffbc2551459940747c4
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-11 16:39:04 +0200

    Actually better without the title

--- src/app/common/show-case/show-case.component.ts
+++ src/app/common/show-case/show-case.component.ts
@@ -16,8 +16,7 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-show-case',
   standalone: true,
   imports: [CommonModule, HighlightAuto],
-  template: ` <h2>Example</h2>
-    <ng-container
+  template: ` <ng-container
       *ngComponentOutlet="cmp.component; inputs: { data: cmp.data }"
     ></ng-container>
     @for(source of sources; track source){
