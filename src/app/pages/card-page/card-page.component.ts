2d24511084ed5d568eef6246cfd2a22c19be1f60
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-11 08:17:28 +0200

    Update card page

--- src/app/pages/card-page/card-page.component.ts
+++ src/app/pages/card-page/card-page.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CardComponent } from '../../common/card/card.component';
 
 @Component({
   selector: 'app-card-page',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>card-page works!</p>`,
+  imports: [CommonModule, CardComponent],
+  template: `<app-card
+    [data]="{ title: 'Simple card', content: 'That displays text content' }"
+  ></app-card>`,
   styleUrl: './card-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardPageComponent { }
+export class CardPageComponent {}
