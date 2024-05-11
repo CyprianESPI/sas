a5b995c4aa918afae39d5006c8ab5eb61c4c69d2
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-11 08:59:50 +0200

    Load raw text file with code content

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHighlightOptions } from 'ngx-highlightjs';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     provideHighlightOptions({
       fullLibraryLoader: () => import('highlight.js'),
     }),
--- src/app/pages/card-page/card-page.component.ts
+++ src/app/pages/card-page/card-page.component.ts
@@ -1,15 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { CardComponent } from '../../common/card/card.component';
+import { HttpClient } from '@angular/common/http';
+import { Highlight } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-card-page',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, Highlight],
   template: `<app-card
-    [data]="{ title: 'Simple card', content: 'That displays text content' }"
-  ></app-card>`,
+      [data]="{ title: 'Simple card', content: 'That displays text content' }"
+    ></app-card>
+
+    <pre><code [highlight]="code" language="ts"></code></pre>`,
   styleUrl: './card-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardPageComponent {}
+export class CardPageComponent implements OnInit {
+  code: string = `...`;
+
+  constructor(
+    private _httpClient: HttpClient,
+    private _cd: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this._httpClient
+      .get('assets/card.component.txt', {
+        responseType: 'text',
+      })
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+          this.code = value;
+          this._cd.markForCheck();
+        },
+      });
+  }
+}
