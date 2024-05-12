daa70dfc60ddbd0ee5b3d214c2b02bb9a9681b72
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-12 13:06:07 +0200

    Update ng container option

--- src/app/pages/ng-container-page/ng-container-page.component.ts
+++ src/app/pages/ng-container-page/ng-container-page.component.ts
@@ -3,19 +3,29 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonComponent } from '../../common/button/button.component';
 import { CardComponent, CardData } from '../../common/card/card.component';
 import { IComponentData } from '../../models/i-component';
+import { ISourceCode } from '../../models/i-source-code';
+import { ShowCaseCodeComponent } from '../../common/show-case-code/show-case-code.component';
 
 @Component({
   selector: 'app-ng-container-page',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, CardComponent],
-  template: ` @for(cmp of components; track cmp){
+  template: `
+    @for(cmp of components; track cmp){
     <ng-container
       *ngComponentOutlet="cmp.component; inputs: { data: cmp.data }"
     >
     </ng-container>
-    }`,
+    }
+    <app-show-case-code [sources]="sources"></app-show-case-code>
+  `,
   styleUrl: './ng-container-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    CardComponent,
+    ShowCaseCodeComponent,
+  ],
 })
 export class NgContainerPageComponent {
   components: IComponentData[] = [
@@ -39,6 +49,54 @@ export class NgContainerPageComponent {
     }),
   ];
 
+  sources: ISourceCode[] = [
+    {
+      name: 'html',
+      code: `@for(cmp of components; track cmp){
+  <ng-container
+    *ngComponentOutlet="cmp.component; inputs: { data: cmp.data }"
+  >
+  </ng-container>
+}`,
+    },
+    {
+      name: 'typescript',
+      code: `components: IComponentData[] = [
+        CardComponent.Make({
+          content: 'content generated from typescript',
+          title: 'TS card',
+        }),
+        ButtonComponent.Make({
+          callBack: () => {
+            this.handleClick('Button 1');
+          },
+          content: 'Button 1',
+          toolTip: 'Adds content to card',
+        }),
+        ButtonComponent.Make({
+          callBack: () => {
+            this.handleClick('Button 2');
+          },
+          content: 'Button 2',
+          toolTip: 'Adds content to card',
+        }),
+      ];
+      
+      handleClick(value: string) {
+        // Update the inputs live
+        const card = this.components[0];
+        if (!card) return;
+        const cardData: CardData = card.data as CardData;
+        if (!cardData) return;
+        card.data = {
+          ...cardData,
+          content: cardData.content + ' ' + value + '...',
+        };
+      }
+      `,
+    },
+  ];
+
   handleClick(value: string) {
     // Update the inputs live
     const card = this.components[0];
@@ -47,7 +105,7 @@ export class NgContainerPageComponent {
     if (!cardData) return;
     card.data = {
       ...cardData,
-      content: `${cardData.content} ${value}...`,
+      content: cardData.content + ' ' + value + '...',
     };
   }
 }
