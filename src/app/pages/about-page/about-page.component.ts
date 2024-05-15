c4cc50084e2324c6c4303f743c763b2ba9500460
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-15 19:59:41 +0200

    Update about page

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -4,11 +4,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>about-page works!</p>`,
+  imports: [CommonModule],
+  template: ` <h2>About</h2>
+    <a href="https://github.com/CyprianESPI/sas" target="_blank">
+      View on GitHub
+    </a>`,
   styleUrl: './about-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutPageComponent { }
+export class AboutPageComponent {}
