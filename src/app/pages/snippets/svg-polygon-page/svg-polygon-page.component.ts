ed4c96d678ff2bbd181fd994ef752c46de3757e3
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-18 11:06:28 +0200

    Svg-polygon page: add source code

--- src/app/pages/snippets/svg-polygon-page/svg-polygon-page.component.ts
+++ src/app/pages/snippets/svg-polygon-page/svg-polygon-page.component.ts
@@ -1,13 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SvgPolygonExampleComponent } from '../../../common/svg/svg-polygon/svg-polygon-example.component';
+import { ShowCaseCodeComponent } from '../../../common/show-case-code/show-case-code.component';
+import { ISourceCode } from '../../../models/i-source-code';
 
 @Component({
   selector: 'app-svg-polygon-page',
   standalone: true,
-  template: `<app-svg-polygon-example></app-svg-polygon-example>`,
+  template: `<app-svg-polygon-example></app-svg-polygon-example>
+    <app-show-case-code [sources]="sources"></app-show-case-code>`,
   styleUrl: './svg-polygon-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, SvgPolygonExampleComponent],
+  imports: [CommonModule, SvgPolygonExampleComponent, ShowCaseCodeComponent],
 })
-export class SvgPolygonPageComponent {}
+export class SvgPolygonPageComponent {
+  sources: ISourceCode[] = [
+    {
+      name: 'common/svg/svg-polygon/svg-polygon-example.component.ts',
+    },
+    {
+      name: 'common/svg/svg-polygon/svg-polygon.component.ts',
+    },
+    {
+      name: 'common/svg/svg-polygon/svg-polygon.component.scss',
+    },
+    {
+      name: 'models/i-polygon.ts',
+    },
+    {
+      name: 'models/i-point.ts',
+    },
+  ];
+}
